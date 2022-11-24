5d6b35756c493f94ad8705905e7013bbd68c0df1
fizfakrak <anton79910161@yandex.ru>
2022-11-24 23:33:01 +0500

    with '#' will be ignored, and an empty message aborts the commit. done Task2#

---
+++ Task002/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите первое число ");
+int input_number_1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int input_number_2 = int.Parse(Console.ReadLine());
+if(input_number_1>input_number_2) Console.Write($"Max = {input_number_1}, Min = {input_number_2}");
+else Console.Write($"Max = {input_number_2}, Min = {input_number_1}");
