5f293722524c32ff7d077c7da273d801f49bdca4
fizfakrak <anton79910161@yandex.ru>
2022-11-24 23:52:14 +0500

    done task 006

---
+++ Task006/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int input_number = int.Parse(Console.ReadLine());
+if(input_number%2 == 0) Console.Write("Число четное");
+else Console.Write("Число нечетное");
