cc75c5a1631f31e5eceb46fc53700ff699fcdd64
fizfakrak <anton79910161@yandex.ru>
2022-11-24 23:57:38 +0500

    done task 008

---
+++ Task008/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int input_number = int.Parse(Console.ReadLine());
+int counter;
+for(counter = 2;counter < input_number; counter = counter + 2 )
+{
+Console.Write($"{counter} ");
+}
